5e6f25241b63707fa1ebf93a8344d6efe3300ac6
Renaldy <renalfrontend@gmail.com>
2020-11-29 21:22:11 +0700

    Commit training REAL 04_useQueryApi UPDATE with ErrorBoundary 100%

--- src/meTutorial/training/04_UseQueryAPI/PostDetail.js
+++ src/meTutorial/training/04_UseQueryAPI/PostDetail.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import usePostDetail from './usePostDetail';
+import {ErrorBoundary} from 'react-error-boundary';
 
 function PostDetail(props) {
     const param = useParams();
@@ -10,21 +11,44 @@ function PostDetail(props) {
 
     return(
         <Fragment>
-            <div className="card">
-                <h2>{detailPost.title}</h2>
-                <p>{detailPost.body}</p>
-            </div>
+                <div className="card">
+                    <ErrorBoundary FallbackComponent={ErrorFallback} onError={MyErrorComponent}>
+                        <Data param={detailPost} />
+                    </ErrorBoundary>
+                </div>
 
-            <button
-                onClick={() => {
-                    props.history.push('/');
-                }}
-            >
-                Back
-            </button>
-
-        </Fragment>
+                <button
+                    onClick={() => {
+                        props.history.push('/');
+                    }}
+                >
+                    Back
+                </button>
+            </Fragment>
     )
 }
+
+function Data({param}) {
+    return(
+        <>
+            <h2>{param.title}</h2>
+            <p>{param.body}</p>
+        </>
+    )
+}
+
+function MyErrorComponent(error, info) {
+    console.log(info);
+}
+
+function ErrorFallback({error, resetErrorBoundary}) {
+    return (
+      <div role="alert">
+        <p>Something went wrong:</p>
+        <pre>{error.message}</pre>
+        <button onClick={resetErrorBoundary}>Try again</button>
+      </div>
+    )
+  }
 
 export default PostDetail;
--- src/meTutorial/training/04_UseQueryAPI/usePostDetail.js
+++ src/meTutorial/training/04_UseQueryAPI/usePostDetail.js
@@ -1,18 +1,19 @@
 import {queryCache, useQuery} from 'react-query';
 
 function GetDetailPost(key, id){
-    return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
-                .then(data => data.json())
-                .then(res => {
-                    return res;
-                })
+    return fetch(`https://jsonplaceholder.typicode.com/posts/131`)
+            .then(data => data.json())
+            .then(res => {    
+                if (res.id === undefined) throw new Error();
+                else return res;
+            })
 }
 
 function usePostDetail(key, id) {
     return useQuery([key, id], GetDetailPost, {
         refetchOnWindowFocus : false,
         retry : 1,
-        enabled : id,
+        enabled : id
     })
 }
 
